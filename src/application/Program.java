caf902ec0aef1b89315e0dcb425315b1ec267f3d
felipeperaltacheung <f33___@hotmail.com>
2021-08-04 16:31:52 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Integer> n = new ArrayList<>();
+		n.add(st);
+		System.out.println(n);
+
+	}
+
+}
