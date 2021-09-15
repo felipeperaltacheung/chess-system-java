1a0e76f6199b7d0c2adf9376051539a54d2b3dd4
felipeperaltacheung <f33___@hotmail.com>
2021-09-15 14:32:35 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,10 +30,12 @@ public class Program {
 			
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			}	
+			
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();	
 			}
+			
 			catch(InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();	
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,14 +1,33 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
+	
 	public Piece(Board board) {
 		this.board = board;
 		position = null;
 	}
 	protected Board getBoard() {
 		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(Position position) { //um metodo concreto
+		return possibleMoves()[position.getRow()][position.getCollum()]; // um metodo abstrato
+	} // ou seja um metodo concreto "possibleMove" utilizando um metodo abstrato "possibleMoves" (Hook methods - metodos de gancho)
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean [][] mat = possibleMoves();
+		for(int i = 0; i<mat.length; i++) {
+			for(int j = 0; j<mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
